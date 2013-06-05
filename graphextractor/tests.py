6c2f186d24eea90e3dd4993c3aec8c39d6319b1b
Drew Vogel <dvogel@github>
2013-06-04 22:05:30 -0400

    Updated tests

--- graphextractor/tests.py
+++ graphextractor/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from graphextractor.tweetparser import TweetLexer
+from graphextractor.tweetparser import TweetLexer, TweetParser
 
 Tweet_0 = "http://anywhere.com #betterthan http://nowhere.com"
 Tweet_1 = "http://duckduckgo.com #isbetterthan http://google.com for #privacy"
@@ -21,21 +21,72 @@ def ilen(itr):
         n += 1
     return n
 
+def token_names_from_string(s):
+    tokens = list(TweetLexer.lex(s))
+    token_names = [tok.name for tok in tokens]
+    return token_names
+
 class GraphExtractorTests(TestCase):
-    def test_lexer_word_count_1(self):
-        """
-        Tests the whitespace splitting.
-        """
-        words = TweetLexer(Tweet_1)
-        cnt = ilen(words)
-        self.assertEqual(cnt, 5)
+    def test_lexer_tokens_0(self):
+        token_names = token_names_from_string(Tweet_0)
+        self.assertEqual(token_names, [u'URL', u'BTHASH', u'URL'])
 
-    def test_lexer_word_count_2(self):
-        """
-        Tests the whitespace splitting.
-        """
-        words = TweetLexer(Tweet_2)
-        cnt = ilen(words)
-        self.assertEqual(cnt, 6)
+    def test_lexer_tokens_1(self):
+        token_names = token_names_from_string(Tweet_1)
+        self.assertEqual(token_names, [u'URL', u'IBTHASH', u'URL',
+                                       u'FOR', u'HASHTAG'])
 
+    def test_lexer_tokens_2(self):
+        token_names = token_names_from_string(Tweet_2)
+        self.assertEqual(token_names, [u'URL', u'IBTHASH', u'URL',
+                                       u'FOR', u'HASHTAG', u'HASHTAG'])
 
+    def test_lexer_tokens_3(self):
+        token_names = token_names_from_string(Tweet_3)
+        self.assertEqual(token_names, [u'WORD', u'WORD', u'WORD',
+                                       u'URL', u'BTHASH', u'URL'])
+
+    def test_lexer_tokens_4(self):
+        token_names = token_names_from_string(Tweet_4)
+        self.assertEqual(token_names, [u'URL', u'BTHASH', u'URL',
+                                       u'WORD', u'WORD', u'WORD', u'WORD'])
+
+    def test_lexer_tokens_5(self):
+        token_names = token_names_from_string(Tweet_5)
+        self.assertEqual(token_names, [u'URL', u'BTHASH', u'URL'])
+
+    def test_parser_structure_0(self):
+        tweet = TweetParser.parse(TweetLexer.lex(Tweet_0))
+        self.assertEqual(tweet, {'better_url': 'http://anywhere.com',
+                                 'worse_url': 'http://nowhere.com',
+                                 'topics': []})
+
+    def test_parser_structure_1(self):
+        tweet = TweetParser.parse(TweetLexer.lex(Tweet_1))
+        self.assertEqual(tweet, {'better_url': 'http://duckduckgo.com',
+                                 'worse_url': 'http://google.com',
+                                 'topics': ['privacy']})
+
+    def test_parser_structure_2(self):
+        tweet = TweetParser.parse(TweetLexer.lex(Tweet_2))
+        self.assertEqual(tweet, {'better_url': 'http://www.opensecrets.org/',
+                                 'worse_url': 'http://fec.gov/',
+                                 'topics': ['campaignfinance', 'opendata']})
+
+    def test_parser_structure_3(self):
+        tweet = TweetParser.parse(TweetLexer.lex(Tweet_3))
+        self.assertEqual(tweet, {'better_url': 'http://a.cz',
+                                 'worse_url': 'http://b.info',
+                                 'topics': []})
+
+    def test_parser_structure_4(self):
+        tweet = TweetParser.parse(TweetLexer.lex(Tweet_4))
+        self.assertEqual(tweet, {'better_url': 'http://a.cz',
+                                 'worse_url': 'http://b.info',
+                                 'topics': []})
+
+    def test_parser_structure_5(self):
+        tweet = TweetParser.parse(TweetLexer.lex(Tweet_5))
+        self.assertEqual(tweet, {'better_url': 'http://anywhere.com/with/a/path/?and=a&query=string',
+                                 'worse_url': 'http://nowhere.com',
+                                 'topics': []})
